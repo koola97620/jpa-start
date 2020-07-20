75ac76f2b084eea103cf4e4bfcc4a5d8b5396bb2
Jaeyong <koola97620@nate.com>
2020-07-20 23:19:45 +0900

    model

--- src/main/java/jpastart/reserve/model/User.java
+++ src/main/java/jpastart/reserve/model/User.java
@@ -1,11 +1,45 @@
 package jpastart.reserve.model;
 
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 /**
  * @author choijaeyong on 2020/07/19.
  * @project jpa-start
  * @description
  */
+@Entity
+@Table(name="user")
 public class User {
 
+  @Id
+  private String email;
+  private String name;
 
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = "create_date")
+  private Date createDate;
+
+  public User(String email, String name, Date createDate) {
+    this.email = email;
+    this.name = name;
+    this.createDate = createDate;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Date getCreateDate() {
+    return createDate;
+  }
 }
